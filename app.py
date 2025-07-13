871da4b5279cee12696631698b12c40df774e544
kunalmeher <52349666+kunalmeher@users.noreply.github.com>
2025-07-13 16:16:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from AWS App Runner!"
+    return "Hello from AWS App Runner!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
